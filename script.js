f041a0aade5b3dbfa65d8ce8b7b8efcbaf89094c
Ignacio <nachofargas23@gmail.com>
2025-09-21 16:13:16 -0300

    Primera Subida

---
+++ script.js
@@ -0,0 +1,13 @@
+const btnTop = document.getElementById("btnTop");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        btnTop.style.display = "block";
+    } else {
+        btnTop.style.display = "none";
+    }
+};
+
+btnTop.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
